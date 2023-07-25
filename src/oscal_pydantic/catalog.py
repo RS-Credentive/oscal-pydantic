6fce3f9105b7f5da58a0a09513c2164dfc662817
RobertSherwood <robert.sherwood@credentive.com>
2023-07-25 14:45:56 +0000

    finished common - created skeleton of control

--- src/oscal_pydantic/catalog.py
+++ src/oscal_pydantic/catalog.py
@@ -2,10 +2,42 @@ from __future__ import annotations
 
 from core import datatypes, common, base
 
-from pydantic import RootModel, Field, field_validator, FieldValidationInfo, AnyUrl
+from pydantic import Field
 
-from oscal_pydantic.core import property
+
+class Parameter(base.OscalModel):
+    pass
+
+
+class Control(base.OscalModel):
+    pass
+
+
+class Group(base.OscalModel):
+    pass
 
 
 class Catalog(base.OscalModel):
     uuid: datatypes.UUID
+    metadata: common.Metadata
+    params: list[Parameter] | None = Field(
+        description="""
+            Parameters provide a mechanism for the dynamic assignment of value(s) in a control.
+        """,
+        default=None,
+    )
+    controls: list[Control] | None = Field(
+        description="""
+            A structured information object representing a security or privacy control. Each 
+            security or privacy control within the Catalog is defined by a distinct control 
+            instance.
+        """,
+        default=None,
+    )
+    groups: list[Group] | None = Field(
+        description="""
+            A group of controls, or of groups of controls.
+        """,
+        default=None,
+    )
+    back_matter: common.BackMatter
--- src/oscal_pydantic/core/common.py
+++ src/oscal_pydantic/core/common.py
@@ -6,11 +6,13 @@
 
 from __future__ import annotations
 from datetime import datetime, timezone
+import re
 
 from . import base, datatypes
 
 from pydantic import (
     Field,
+    field_validator,
 )
 
 
@@ -238,6 +240,78 @@ class Role(base.OscalModel):
     )
 
 
+class Address(base.OscalModel):
+    type: datatypes.Token | None = Field(
+        description="""
+            Indicates the type of address.
+        """,
+        default=None,
+    )
+    addr_lines: list[datatypes.String] | None = Field(
+        description="""
+            A list of Address Line: A single line of an address.       
+        """,
+        default=None,
+    )
+    city: datatypes.String | None = Field(
+        description="""
+            City, town or geographical region for the mailing address.
+        """,
+        default=None,
+    )
+    state: datatypes.String | None = Field(
+        description="""
+                State, province or analogous geographical region for mailing address
+            """,
+        default=None,
+    )
+    postal_code: datatypes.String | None = Field(
+        description="""
+            Postal or ZIP code for mailing address
+        """,
+        default=None,
+    )
+    country: datatypes.String | None = Field(
+        description="""
+            The ISO 3166-1 alpha-2 country code for the mailing address.
+        """,
+        default=None,
+    )
+
+    @field_validator("country")
+    def two_letter_country_code(cls, v: datatypes.String):
+        if re.match("[A-Z]{2}", v.root) == None:
+            raise ValueError("country string must have 2 letters")
+
+
+class TelephoneNumber(base.OscalModel):
+    """A class to represent a telephone number in OSCAL
+
+    Attributes:
+        type (datatype.String): the type of phone number
+        number (datatype.String): the phone number
+    """
+
+    type: datatypes.String | None = Field(
+        description="""
+                Indicates the type of phone number. 
+
+                The value may be locally defined, or one of the following:
+
+                home: A home phone number.
+                office: An office phone number.
+                mobile: A mobile phone number.
+            """,
+        default=None,
+    )
+    number: datatypes.String | None = Field(
+        description="""
+            The phone number
+        """,
+        default=None,
+    )
+
+
 class Location(base.OscalModel):
     # DESCRIPTION A location, with associated metadata that can be referenced.
 
@@ -259,7 +333,228 @@ class Location(base.OscalModel):
     # primary: The location is a data-center used for normal operations.
     # alternate: The location is a data-center used for fail-over or backup operations.
 
-    uuid: datatypes.UUID
+    uuid: datatypes.UUID = Field(
+        description="""
+            A machine-oriented, globally unique identifier with cross-instance scope that can be 
+            used to reference this defined location elsewhere in this or other OSCAL instances. 
+            The locally defined UUID of the location can be used to reference the data item 
+            locally or globally (e.g., from an importing OSCAL instance). This UUID should be 
+            assigned per-subject, which means it should be consistently used to identify the same 
+            subject across revisions of the document.
+        """
+    )
+    title: datatypes.MarkupLine | None = Field(
+        description="""
+            A name given to the location, which may be used by a tool for display and navigation.
+        """,
+        default=None,
+    )
+    address: Address = Field(
+        description="""
+            A postal Address for the location
+        """,
+    )
+    email_addresses: list[datatypes.Email] | None = Field(
+        description="""
+            An email address as defined by RFC 5322 Section 3.4.1
+        """,
+        default=None,
+    )
+    telephone_numbers: list[TelephoneNumber] | None = Field(
+        description="""
+            A list of telephone numbers for the locations
+        """,
+        default=None,
+    )
+    urls: list[datatypes.Uri] | None = Field(
+        description="""
+            The uniform resource locator (URL) for a web site or Internet presence associated with 
+            the location.
+        """,
+        default=None,
+    )
+    props: list[Property] | None = Field(
+        description="""
+            An attribute, characteristic, or quality of the containing object expressed as a 
+            namespace qualified name/value pair. The value of a property is a simple scalar value, 
+            which may be expressed as a list of values.
+        """,
+        default=None,
+    )
+    links: list[Link] | None = Field(
+        description="""
+            A reference to a local or remote resource
+        """,
+        default=None,
+    )
+    remarks: datatypes.MarkupMultiline | None = Field(
+        description="""
+            Additional commentary on the containing object.
+        """,
+        default=None,
+    )
+
+
+class ExternalID(base.OscalModel):
+    """An identifier for a person or organization using a designated scheme. e.g. an Open Researcher and Contributor ID (ORCID)
+
+    Attributes:
+        scheme (datatypes.Uri): Indicates the type of external identifier.
+        id (datattypes.String): the ID of the party"""
+
+    scheme: datatypes.Uri = Field(
+        description="""
+            Indicates the type of external identifier.
+        """,
+    )
+    id: datatypes.String | None = Field(
+        description="""
+            An identifier for a person or organization using a designated scheme. e.g. an Open Researcher and Contributor ID (ORCID)
+        """,
+        default=None,
+    )
+
+
+class Party(base.OscalModel):
+    """A class representing an OSCAL Party
+
+    Attributes:
+        param name (param type): describe the param
+    """
+
+    uuid: datatypes.UUID = Field(
+        description="""
+            A machine-oriented, globally unique identifier with cross-instance scope that can be 
+            used to reference this defined party elsewhere in this or other OSCAL instances. The 
+            locally defined UUID of the party can be used to reference the data item locally or 
+            globally (e.g., from an importing OSCAL instance). This UUID should be assigned per-
+            subject, which means it should be consistently used to identify the same subject 
+            across revisions of the document.
+        """,
+    )
+    type: datatypes.String = Field(
+        description="""
+            A category describing the kind of party the object describes.
+
+            Constraint (1)
+            ALLOWED VALUES
+
+            The value must be one of the following:
+
+            person: An individual.
+            organization: A group of individuals formed for a specific purpose.
+        """,
+    )
+    name: datatypes.String | None = Field(
+        description="""
+            The full name of the party. This is typically the legal name associated with the party.
+        """,
+        default=None,
+    )
+    short_name: datatypes.String | None = Field(
+        description="""
+            A short common name, abbreviation, or acronym for the party.
+        """,
+        default=None,
+    )
+    external_ids: list[ExternalID] | None = Field(
+        description="""
+            An identifier for a person or organization using a designated scheme. e.g. an Open 
+            Researcher and Contributor ID (ORCID)
+        """,
+        default=None,
+    )
+    props: list[Property] | None = Field(
+        description="""
+            Additional properties related to the party
+        """,
+        default=None,
+    )
+    links: list[Link] | None = Field(
+        description="""
+            A list of references to local or remote resources.
+        """,
+        default=None,
+    )
+    email_addresses: list[datatypes.Email] | None = Field(
+        description="""
+            A list of email addresses
+        """,
+        default=None,
+    )
+    telephone_numbers: list[TelephoneNumber] | None = Field(
+        description="""
+            A list of telephone numbers
+        """,
+        default=None,
+    )
+    addresses: list[Address] | None = Field(
+        description="""
+            A list of postal addresses
+        """,
+        default=None,
+    )
+    location_uuids: list[datatypes.UUID] | None = Field(
+        description="""
+            A list of identifier references to a location defined in the metadata section of this or another OSCAL document.
+        """,
+        default=None,
+    )
+    member_of_organizations: list[datatypes.UUID] | None = Field(
+        description="""
+            A list of machine-oriented references to partise that this subject is associated with.
+        """,
+        default=None,
+    )
+    remarks: list[datatypes.MarkupMultiline] | None = Field(
+        description="""
+            Additional commentary on the containing object.
+        """,
+        default=None,
+    )
+
+    @field_validator("type")
+    def permitted_types(cls, v: datatypes.String):
+        if v not in [
+            datatypes.String(root="person"),
+            datatypes.String(root="organization"),
+        ]:
+            raise ValueError('Party.type must be "person" or "organization"')
+
+
+class ResponsibleParty(base.OscalModel):
+    role_id: datatypes.Token = Field(
+        description="""
+            A human-oriented identifier reference to roles served by the user
+        """,
+    )
+    party_uuids: list[datatypes.UUID] = Field(
+        description="""
+            A machine-oriented identifier reference to another party defined in metadata. 
+            The UUID of the party in the source OSCAL instance is sufficient to reference the data 
+            item locally or globally (e.g., in an imported OSCAL instance).
+        """,
+    )
+    props: list[Property] | None = Field(
+        description="""
+            An attribute, characteristic, or quality of the containing object expressed as a 
+            namespace qualified name/value pair. The value of a property is a simple scalar value,
+            which may be expressed as a list of values.
+        """,
+        default=None,
+    )
+    links: list[Link] | None = Field(
+        description="""
+            A reference to a local or remote resource
+        """,
+        default=None,
+    )
+    remarks: datatypes.MarkupMultiline | None = Field(
+        description="""
+            Additional commentary on the containing object.
+        """,
+        default=None,
+    )
 
 
 class Metadata(base.OscalModel):
@@ -325,6 +620,199 @@ class Metadata(base.OscalModel):
     roles: list[Role] | None = Field(
         description="""
             Defines a function assumed or expected to be assumed by a party in a specific situation.
+        """,
+        default=None,
+    )
+    locations: list[Location] | None = Field(
+        description="""
+            A list of locations, with associated metadata that can be referenced.
+        """,
+        default=None,
+    )
+    parties: list[Party] | None = Field(
+        description="""
+            A list of responsible entities, each of which is either a person or an organization.
+        """,
+        default=None,
+    )
+    responsible_parties: list[ResponsibleParty] | None = Field(
+        description="""
+            A reference to a set of organizations or persons that have responsibility for 
+            performing a referenced role in the context of the containing object.
+        """,
+        default=None,
+    )
+    remarks: datatypes.MarkupMultiline | None = Field(
+        description="""
+            Additional commentary on the containing object.
+        """,
+        default=None,
+    )
+
+
+class Citation(base.OscalModel):
+    text: datatypes.MarkupLine = Field(
+        description="""
+            A line of citation text.
+        """,
+    )
+    props: list[Property] | None = Field(
+        description="""
+            An optional list of attributes, characteristics, or qualities of the containing object 
+            expressed as a namespace qualified name/value pair. The value of a property is a simple 
+            scalar value, which may be expressed as a list of values.
+        """,
+        default=None,
+    )
+    links: list[Link] | None = Field(
+        description="""
+            An optional list of references to a local or remote resource.
+        """,
+        default=None,
+    )
+
+
+class Hash(base.OscalModel):
+    algorithm: datatypes.String = Field(
+        description="""
+            Method by which a hash is derived
+
+            Remarks
+            Any other value used MUST be a value defined in the W3C XML Security Algorithm Cross-
+            Reference Digest Methods (W3C, April 2013) or RFC 6931 Section 2.1.5 New SHA Functions.
+
+            Constraint (1)
+            ALLOWED VALUES
+
+            The value may be locally defined, or one of the following:
+
+            SHA-224: The SHA-224 algorithm as defined by NIST FIPS 180-4.
+            SHA-256: The SHA-256 algorithm as defined by NIST FIPS 180-4.
+            SHA-384: The SHA-384 algorithm as defined by NIST FIPS 180-4.
+            SHA-512: The SHA-512 algorithm as defined by NIST FIPS 180-4.
+            SHA3-224: The SHA3-224 algorithm as defined by NIST FIPS 202.
+            SHA3-256: The SHA3-256 algorithm as defined by NIST FIPS 202.
+            SHA3-384: The SHA3-384 algorithm as defined by NIST FIPS 202.
+            SHA3-512: The SHA3-512 algorithm as defined by NIST FIPS 202.
+        """,
+        default=None,
+    )
+    value: datatypes.String = Field(
+        description="""
+            The value of the hash
+        """,
+        default=None,
+    )
+
+
+class ResourceLink(base.OscalModel):
+    href: datatypes.UriReference = Field(
+        description="""
+            A resolvable URI reference to a resource.
+        """,
+    )
+    media_type: datatypes.String = Field(
+        description="""
+            Specifies a media type as defined by the Internet Assigned Numbers Authority (IANA) 
+            Media Types Registry.
+        """,
+        default=None,
+    )
+    hashes: list[Hash] = Field(
+        description="""
+            description
+        """,
+        default=None,
+    )
+
+
+class Base64(base.OscalModel):
+    filename: datatypes.UriReference = Field(
+        description="""
+            Name of the file before it was encoded as Base64 to be embedded in a resource. This is 
+            the name that will be assigned to the file when the file is decoded.
+        """,
+        default=None,
+    )
+    media_type: datatypes.String = Field(
+        description="""
+            Specifies a media type as defined by the Internet Assigned Numbers Authority (IANA) 
+            Media Types Registry.
+        """,
+        default=None,
+    )
+    value: datatypes.Base64Binary = Field(
+        description="""
+            The Base64 encoded file.
+        """,
+        default=None,
+    )
+
+
+class Resource(base.OscalModel):
+    uuid: datatypes.UUID = Field(
+        description="""
+            A machine-oriented, globally unique identifier with cross-instance scope that can be 
+            used to reference this defined resource elsewhere in this or other OSCAL instances. 
+            This UUID should be assigned per-subject, which means it should be consistently used 
+            to identify the same subject across revisions of the document.
+        """,
+    )
+    title: datatypes.MarkupLine | None = Field(
+        description="""
+            A name given to the resource, which may be used by a tool for display and navigation.
+        """,
+        default=None,
+    )
+    description: datatypes.MarkupMultiline | None = Field(
+        description="""
+            A short summary of the resource used to indicate the purpose of the resource.
+        """,
+        default=None,
+    )
+    props: list[Property] | None = Field(
+        description="""
+            An optional list of properties associated with the resource.
+        """,
+        default=None,
+    )
+    document_ids: list[DocumentID] | None = Field(
+        description="""
+            An optional list of document Identifiers associated with the resource
+        """,
+        default=None,
+    )
+    citation: Citation | None = Field(
+        description="""
+            A citation consisting of end note text and optional structured bibliographic data.
+        """,
+        default=None,
+    )
+    rlinks: list[ResourceLink] | None = Field(
+        description="""
+            An optional list of pointers to an external resource with an optional hash for 
+            verification and change detection.
+        """,
+        default=None,
+    )
+    base64: list[Base64] | None = Field(
+        description="""
+            A resource encoded in Base64 and embedded in the document
+        """,
+        default=None,
+    )
+    remarks: datatypes.MarkupMultiline | None = Field(
+        description="""
+            Additional commentary on the containing object.
+        """,
+        default=None,
+    )
+
+
+class BackMatter(base.OscalModel):
+    resources: list[Resource] | None = Field(
+        description="""
+            A collection of resources, which may be included directly or by reference.
         """,
         default=None,
     )
