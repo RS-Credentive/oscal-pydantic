a4fa7433f68bad90f25ae41fa90c21801c85224e
Robert Sherwood <122729488+RS-Credentive@users.noreply.github.com>
2023-12-12 17:40:23 +0000

    Test cases for import JSON

---
+++ tests/test_import.py
@@ -0,0 +1,30 @@
+from oscal_pydantic import document, catalog
+from pathlib import Path
+
+RESOURCES_PATH = Path(__file__).parent.joinpath("resources")
+
+
+class TestImport:
+    def test_import_nested_part(self):
+        with open(RESOURCES_PATH.joinpath(Path("nested_part.json"))) as part_file:
+            part_bytes = part_file.read()
+        test_part = catalog.BasePart.model_validate_json(part_bytes)
+
+        assert isinstance(
+            catalog.StatementItemPart.model_validate(test_part.model_dump()),
+            catalog.StatementItemPart,
+        )
+
+    def test_import_statement_part(self):
+        with open(RESOURCES_PATH.joinpath(Path("statement_part.json"))) as part_file:
+            part_bytes = part_file.read()
+        test_part = catalog.StatementPart.model_validate_json(part_bytes)
+        assert isinstance(test_part, catalog.StatementPart)
+
+    def test_import_catalog(self):
+        with open(
+            RESOURCES_PATH.joinpath(Path("NIST_SP-800-53_rev5_catalog.json"))
+        ) as catalog_file:
+            catalog_bytes = catalog_file.read()
+        test_catalog = document.Document.model_validate_json(catalog_bytes)
+        assert isinstance(test_catalog, document.Document)
