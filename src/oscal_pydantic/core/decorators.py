0a6ef7ccebb5a941dfca6cbbce33589b6f296fc1
Robert Sherwood <122729488+RS-Credentive@users.noreply.github.com>
2023-12-16 11:59:32 +0000

    playing with decorators

---
+++ src/oscal_pydantic/core/decorators.py
@@ -0,0 +1,12 @@
+from typing import Callable, Any
+from . import base
+
+import functools
+
+
+def restricted_class(cls):
+    @functools.wraps(cls)
+    def get_class_restrictions(*args, **kwargs):
+        get_allowed_values: list[base.AllowedValue] = cls.get_allowed_values()
+        if cls == base.BaseModel:
+            return
