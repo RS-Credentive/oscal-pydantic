c1758732acf32513d2aa9559cd3a714c087d98fb
Peter Burkholder <peter.burkholder@gsa.gov>
2023-12-28 10:14:30 -0500

    Remove extraneous spaces

--- src/oscal_pydantic/profile.py
+++ src/oscal_pydantic/profile.py
@@ -6,7 +6,7 @@ from pydantic import Field, field_validator, AnyUrl
 
 import warnings
 
-class Profile (base.OscalModel):
+class Profile(base.OscalModel):
     uuid: datatypes.OscalUUID
     metadata: common.Metadata
     # imports:
@@ -14,17 +14,17 @@ class Profile (base.OscalModel):
     # modify:
     back_matter: common.BackMatter
 
-class Import (base.OscalModel):
+class Import(base.OscalModel):
     href: datatypes.OscalUriReference
     # include_all: We will implement this if/when we find such an example
     include_controls: list[ChildControl] | None
     exclude_controls: list[ChildControl] | None
 
-class ChildControl (base.OscalModel):
+class ChildControl(base.OscalModel):
     with_child_controls: datatypes.OscalToken | None
     with_ids: list[datatypes.OscalToken] | None
     matching: list[WildcardPattern] | None
 
-class WildcardPattern (base.OscalModel):
+class WildcardPattern(base.OscalModel):
     pattern: datatypes.OscalString | None
 
